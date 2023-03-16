4f2443b86b533fed070fa16e1181a882c546baab
diane-mulder <127489403+diane-mulder@users.noreply.github.com>
2023-03-16 13:06:06 +0100

    jour1

---
+++ horloge.essai1.py
@@ -0,0 +1,18 @@
+import time
+
+# en informatique, le point de départ du calcul du temps est le 1er janv 1970 à 00h00
+# print(time.time())
+
+while True: 
+
+    heures = "%H"
+    minutes = "%M"
+    secondes = "%S"
+    print(time.strftime("%H:%M:%S"))
+    time.sleep(1)
+    
+
+
+
+
+    
