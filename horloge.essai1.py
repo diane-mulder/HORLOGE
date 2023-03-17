0b6de039d1abc8f8b596e428fce55f005f8f67b8
diane-mulder <127489403+diane-mulder@users.noreply.github.com>
2023-03-17 12:09:52 +0100

    création alarme

--- horloge.essai1.py
+++ horloge.essai1.py
@@ -4,7 +4,7 @@ def afficher_heure():
 
     heure = (16,59,40)
     heure_list = list(heure)
-    alarme = (17,59,00)
+    alarme = (17,00,00)
     alarme_list = list(alarme)
 
     while True:
@@ -15,22 +15,17 @@ def afficher_heure():
             if int(heure_list[1]) == 60:
                 heure_list[1] = 0
                 heure_list[0] += 1
-        print(f"{heure_list[0]}:{heure_list[1]}:{heure_list[2]}")
+        print(f"{heure_list[0]:02}:{heure_list[1]:02}:{heure_list[2]:02}")
+
+        if alarme_list == heure_list:
+            print("c'est l'heure de se reveiller!")
+            break
         
         time.sleep(1)
 
-        if int(alarme_list)[2] ==60:
-            alarme_list[2] = 0
-            
-
-
+               
 
 afficher_heure()       
 
-
-
-                
-
-   
 
 
